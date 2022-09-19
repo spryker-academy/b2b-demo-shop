7c45eeb141fb051fc3f86de0b53693a3901841a6
Florian Scholz <florian.scholz@spryker.com>
2022-09-19 19:32:39 +0200

    Adds example for table and form

---
+++ src/Pyz/Zed/Antelope/Business/Writer/AntelopeWriter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Pyz\Zed\Antelope\Business\Writer;
+
+use Generated\Shared\Transfer\AntelopeTransfer;
+use Pyz\Zed\Antelope\Persistence\AntelopeEntityManagerInterface;
+
+class AntelopeWriter
+{
+    /**
+     * @var \Pyz\Zed\Antelope\Persistence\AntelopeEntityManagerInterface
+     */
+    protected $antelopeEntityManager;
+
+    /**
+     * @param \Pyz\Zed\Antelope\Persistence\AntelopeEntityManagerInterface $antelopeEntityManager
+     */
+    public function __construct(AntelopeEntityManagerInterface $antelopeEntityManager)
+    {
+        $this->antelopeEntityManager = $antelopeEntityManager;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\AntelopeTransfer $antelopeTransfer
+     *
+     * @return \Generated\Shared\Transfer\AntelopeTransfer
+     */
+    public function create(AntelopeTransfer $antelopeTransfer): AntelopeTransfer
+    {
+        return $this->antelopeEntityManager->createAntelope($antelopeTransfer);
+    }
+}
---
+++ src/Pyz/Zed/AntelopeGui/Communication/AntelopeGuiCommunicationFactory.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Pyz\Zed\AntelopeGui\Communication;
+
+use Generated\Shared\Transfer\AntelopeTransfer;
+use Orm\Zed\Antelope\Persistence\PyzAntelopeQuery;
+use Pyz\Zed\Antelope\Business\AntelopeFacadeInterface;
+use Pyz\Zed\AntelopeGui\AntelopeGuiDependencyProvider;
+use Pyz\Zed\AntelopeGui\Communication\Form\AntelopeCreateForm;
+use Pyz\Zed\AntelopeGui\Communication\Table\AntelopeTable;
+use Spryker\Zed\Kernel\Communication\AbstractCommunicationFactory;
+use Symfony\Component\Form\FormInterface;
+
+class AntelopeGuiCommunicationFactory extends AbstractCommunicationFactory
+{
+    /**
+     * @return \Pyz\Zed\AntelopeGui\Communication\Table\AntelopeTable
+     */
+    public function createAntelopeTable()
+    {
+        return new AntelopeTable(
+            $this->getAntelopePropelQuery()
+        );
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\AntelopeTransfer $antelopeTransfer
+     * @param array<string, mixed> $options
+     *
+     * @return \Symfony\Component\Form\FormInterface
+     */
+    public function createAntelopeCreateForm(AntelopeTransfer $antelopeTransfer, array $options = []): FormInterface
+    {
+        return $this->getFormFactory()->create(AntelopeCreateForm::class, $antelopeTransfer, $options);
+    }
+
+    /**
+     * @return \Pyz\Zed\Antelope\Business\AntelopeFacadeInterface
+     */
+    public function getAntelopeFacade(): AntelopeFacadeInterface
+    {
+        return $this->getProvidedDependency(AntelopeGuiDependencyProvider::FACADE_ANTELOPE);
+    }
+
+    /**
+     * @return \Orm\Zed\Antelope\Persistence\PyzAntelopeQuery
+     */
+    public function getAntelopePropelQuery(): PyzAntelopeQuery
+    {
+        return $this->getProvidedDependency(AntelopeGuiDependencyProvider::PROPEL_QUERY_ANTELOPE);
+    }
+}
