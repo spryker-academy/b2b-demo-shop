b006fd709f4da5add932b5265b7ab459bafd5b8d
Florian Schwarzmeier <florian.schwarzmeier@spryker.com>
2022-12-15 14:57:20 +0100

    Module Layers - Completed

---
+++ src/Pyz/Client/Training/TrainingDependencyProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Pyz\Client\Training;
+
+use Spryker\Client\Kernel\AbstractDependencyProvider;
+use Spryker\Client\Kernel\Container;
+
+class TrainingDependencyProvider extends AbstractDependencyProvider
+{
+    public const CLIENT_ZED_REQUEST = 'CLIENT_ZED_REQUEST';
+
+    public const CLIENT_STORE = 'CLIENT_STORE';
+
+    public function provideServiceLayerDependencies(Container $container): Container
+    {
+        $container = $this->addZedRequestClient($container);
+        $container = $this->addStoreClient($container);
+
+        return $container;
+    }
+
+    protected function addZedRequestClient(Container $container): Container
+    {
+        $container->set(static::CLIENT_ZED_REQUEST, function (Container $container) {
+            return $container->getLocator()->zedRequest()->client();
+        });
+
+        return $container;
+    }
+
+    protected function addStoreClient(Container $container): Container
+    {
+        $container->set(static::CLIENT_STORE, function (Container $container) {
+            return $container->getLocator()->store()->client();
+        });
+
+        return $container;
+    }
+}
