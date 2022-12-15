9259c7781a2041c5bd636459740722534fbc2088
Florian Schwarzmeier <florian.schwarzmeier@spryker.com>
2022-12-15 14:57:20 +0100

    Module Layers - Back Office Completed

---
+++ src/Pyz/Client/Training/Stub/TrainingStub.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Pyz\Client\Training\Stub;
+
+use Generated\Shared\Transfer\AntelopeCriteriaTransfer;
+use Generated\Shared\Transfer\AntelopeResponseTransfer;
+use Spryker\Client\ZedRequest\ZedRequestClientInterface;
+
+class TrainingStub
+{
+    /**
+     * @var \Spryker\Client\ZedRequest\ZedRequestClientInterface
+     */
+    protected ZedRequestClientInterface $zedRequestClient;
+
+    /**
+     * @param \Spryker\Client\ZedRequest\ZedRequestClientInterface $zedRequestClient
+     */
+    public function __construct(ZedRequestClientInterface $zedRequestClient)
+    {
+        $this->zedRequestClient = $zedRequestClient;
+    }
+
+    public function findAntelope(AntelopeCriteriaTransfer $antelopeCriteria): AntelopeResponseTransfer
+    {
+        /** @var \Generated\Shared\Transfer\AntelopeResponseTransfer $antelopeResponseTransfer */
+        $antelopeResponseTransfer = $this->zedRequestClient->call('/training/gateway/find-antelope', $antelopeCriteria);
+
+        return $antelopeResponseTransfer;
+    }
+}
