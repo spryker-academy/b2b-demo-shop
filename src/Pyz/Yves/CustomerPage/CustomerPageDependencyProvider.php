75640ebdbbeb940e68cdb189f8a140765df8e76e
Florian Scholz <florian.scholz@spryker.com>
2022-12-21 11:53:32 +0100

    Extending Core Modules - Show antelope in profile step

--- src/Pyz/Yves/CustomerPage/CustomerPageDependencyProvider.php
+++ src/Pyz/Yves/CustomerPage/CustomerPageDependencyProvider.php
@@ -26,6 +26,8 @@ class CustomerPageDependencyProvider extends SprykerShopCustomerPageDependencyPr
      */
     public const PYZ_CLIENT_SESSION = 'PYZ_CLIENT_SESSION';
 
+    public const CLIENT_TRAINING = 'CLIENT_TRAINING';
+
     /**
      * @param \Spryker\Yves\Kernel\Container $container
      *
@@ -36,12 +38,19 @@ class CustomerPageDependencyProvider extends SprykerShopCustomerPageDependencyPr
         $container = parent::provideDependencies($container);
 
         $container = $this->addPyzSessionClient($container);
+        $container = $this->addTrainingClient($container);
 
         return $container;
     }
 
-    // TODO: Add the method `addTrainingClient` and call it in the `provideDependencies()`
-    // Hint: The same was already done for another module here: `src/Pyz/Yves/TrainingPage/TrainingPageDependencyProvider.php`
+    protected function addTrainingClient(Container $container): Container
+    {
+        $container->set(static::CLIENT_TRAINING, function (Container $container) {
+            return $container->getLocator()->training()->client();
+        });
+
+        return $container;
+    }
 
     /**
      * @return array<string>
