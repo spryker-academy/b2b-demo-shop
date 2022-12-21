7f1cc03b87e741b1f5f735489989fa477698f438
Florian Schwarzmeier <florian.schwarzmeier@spryker.com>
2022-12-21 11:26:52 +0100

    Extending Core Modules - Skeleton

--- src/Pyz/Yves/CustomerPage/CustomerPageFactory.php
+++ src/Pyz/Yves/CustomerPage/CustomerPageFactory.php
@@ -7,6 +7,9 @@
 
 namespace Pyz\Yves\CustomerPage;
 
+use Pyz\Client\Training\TrainingClientInterface;
+use Pyz\Yves\CustomerPage\Form\FormFactory;
+use Pyz\Yves\CustomerPage\Form\Transformer\AntelopeTransformer;
 use Spryker\Client\Session\SessionClientInterface;
 use SprykerShop\Yves\CustomerPage\CustomerPageFactory as SprykerCustomerPageFactory;
 
@@ -19,4 +22,17 @@ class CustomerPageFactory extends SprykerCustomerPageFactory
     {
         return $this->getProvidedDependency(CustomerPageDependencyProvider::PYZ_CLIENT_SESSION);
     }
+
+    public function getTrainingClient(): TrainingClientInterface
+    {
+        // TODO-1: Get the provided dependency for the TrainingClient
+        // Hint-1: Have a look at src/Pyz/Client/Training/TrainingFactory.php::getZedRequestClient() for the right syntax
+        // Hint-2: The name of the constant to use is 'CustomerPageDependencyProvider::CLIENT_TRAINING'
+    }
+
+    // TODO-2: Instantiate and return the AntelopeTransformer to make it available inside the module
+    // Hint: Naming convention for methods creating instances of a class: createNameOfTheClass()
+
+    // TODO-3: Override createCustomerFormFactory() from SprykerCustomerPageFactory
+    // and make it return our newly created FormFactory instead of the FormFactory of the core
 }
