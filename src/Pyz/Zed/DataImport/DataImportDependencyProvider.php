094292252ed63364f23c402ba9fd4173f209d710
Florian Schwarzmeier <Florian.Schwarzmeier@spryker.com>
2023-03-30 11:39:58 +0200

    Data Import - Complete

--- src/Pyz/Zed/DataImport/DataImportDependencyProvider.php
+++ src/Pyz/Zed/DataImport/DataImportDependencyProvider.php
@@ -358,7 +358,7 @@ class DataImportDependencyProvider extends SprykerDataImportDependencyProvider
             new CategoryStoreDataImportPlugin(),
             new ProductConfigurationDataImportPlugin(),
 
-            // TODO: Add the DataImportPlugin here
+            new AntelopeDataImportPlugin(),
         ];
     }
 
