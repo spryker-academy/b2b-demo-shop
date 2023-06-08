61481dc3d8be0a1de1418e62ff2df394d57d0018
Florian Schwarzmeier <Florian.Schwarzmeier@spryker.com>
2023-06-08 11:08:19 +0200

    Search - Complete

--- src/Pyz/Yves/Router/RouterDependencyProvider.php
+++ src/Pyz/Yves/Router/RouterDependencyProvider.php
@@ -145,7 +145,7 @@ class RouterDependencyProvider extends SprykerRouterDependencyProvider
             new OrderCancelWidgetRouteProviderPlugin(),
             new PaymentPageRouteProviderPlugin(),
             new ProductConfiguratorGatewayPageRouteProviderPlugin(),
-            // TODO: Add the RouteProviderPlugin of the AntelopePage module
+            new AntelopePageRouteProviderPlugin(),
         ];
     }
 
